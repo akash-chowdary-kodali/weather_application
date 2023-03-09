bdb82da007cd786d3b1ef3abd676870ce40d2065
akodali9 <akashkodali6@gmail.com>
2023-03-09 22:32:06 +0530

    added open weather api and making calls

--- src/api.js
+++ src/api.js
@@ -1,9 +1,12 @@
 export const geoApioptions = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'e3638dc10bmshd59130354bdfd2dp1ee862jsn50e897b98c41',
-		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-	}
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "e3638dc10bmshd59130354bdfd2dp1ee862jsn50e897b98c41",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
 };
 
 export const Geo_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "2f92f60d101cacffc85aa878614883ec";
