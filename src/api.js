e14645cd2482d0395d5bfea14c6d69397a015f45
Akash Kodali <akashkodali6@gmail.com>
2023-06-15 21:42:00 +0530

    test

--- src/api.js
+++ src/api.js
@@ -1,9 +1,9 @@
-
+// require('dotenv').config();
 
 export const geoApioptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "e3638dc10bmshd59130354bdfd2dp1ee862jsn50e897b98c41",
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
@@ -11,4 +11,4 @@ export const geoApioptions = {
 export const Geo_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = "2f92f60d101cacffc85aa878614883ec";
+export const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY ;
